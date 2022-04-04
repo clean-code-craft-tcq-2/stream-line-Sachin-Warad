4f1fd5a835dedccb387f7020d07f60bf73c9424e
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-04-05 00:23:59 +0530

    Create SendBatteryParameters.c

---
+++ SendBatteryParameters.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SendBatteryParameters.h"
+
+void printOnConsole(double soc, double temp) {
+        printf("%lf\t%lf\n",soc,temp);
+}
+
+int countParametersNumber(File *fp) {
+  char c;
+  int count = 0;
+  for (c = getc(fp); c != EOF; c = getc(fp))
+  {
+        if (c == '\n') {
+            count = count + 1;
+        }
+  }
+  return count;
+}
+
+int readParametersFromFile(void)
+{
+  FILE *fp;
+  fp = fopen("BatteryParameters.txt", "r");
+  if(fp != NULL)
+  {
+    int numberofParameters = countParametersNumber(fp);
+    return 1;
+  }
+  return 0;
+}
+
+int readAndPrintParametsOnConsole(void) 
+{
+  double soc = 4, temp = 90;
+  int status = readParametersFromFile();
+  printOnConsole(soc,temp)
+  return status;
+}
