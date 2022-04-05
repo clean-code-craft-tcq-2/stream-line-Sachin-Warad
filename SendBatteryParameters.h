1777748f3b2aee098bb75060a9ced19658b14364
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-04-05 23:39:13 +0530

    Update SendBatteryParameters.h

--- SendBatteryParameters.h
+++ SendBatteryParameters.h
@@ -3,5 +3,5 @@
 #define STATUS_NotOK 0
 
 void printOnConsole(double soc, double temp);
-int readParametersFromFile(double temp[], double soc[], char *fileName)
+int readParametersFromFile(double temp[], double soc[], char *fileName);
 int readAndPrintParamets(double temp[], double soc[], void (*fn_ptrPrintOutput)(double temp, double soc), char *fileName);
