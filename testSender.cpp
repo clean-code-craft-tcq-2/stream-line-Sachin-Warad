5f0d65e4a698dd70898350dcc673c644124fd0dc
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-04-05 23:50:35 +0530

    Update testSender.cpp

--- testSender.cpp
+++ testSender.cpp
@@ -3,11 +3,20 @@
 #include "test/catch.hpp"
 #include "SendBatteryParameters.h"
 
-TEST_CASE("Verify Read parameters from file") {
+TEST_CASE("Verify Read parameters from file - Valid Case") {
   double temp[NoOfSamples], soc[NoOfSamples];
   void (*fn_ptrPrintOutput)(double, double);
   fn_ptrPrintOutput = &printOnConsole;
   char fileName[50] = "BatteryParameters.txt";
   int readStatus = readAndPrintParamets(temp,soc,fn_ptrPrintOutput,fileName);
   REQUIRE(readStatus == STATUS_OK);
+}
+
+TEST_CASE("Verify Read parameters from file - InValid Case") {
+  double temp[NoOfSamples], soc[NoOfSamples];
+  void (*fn_ptrPrintOutput)(double, double);
+  fn_ptrPrintOutput = &printOnConsole;
+  char fileName[50] = "BatteryParameters2.txt";
+  int readStatus = readAndPrintParamets(temp,soc,fn_ptrPrintOutput,fileName);
+  REQUIRE(readStatus == STATUS_NotOK);
 }
